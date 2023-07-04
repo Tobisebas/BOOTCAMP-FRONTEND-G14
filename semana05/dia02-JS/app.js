377f5c05c5b2df24d20045db22acdc191d4d25b1
Tobias Abregu <a20213174@pucp.edu.pe>
2023-07-04 03:37:01 -0500

    reto completado

---
+++ semana05/dia02-JS/app.js
@@ -0,0 +1,69 @@
+console.log("hola mundo");
+console.log('Hola G14');
+console.log(2 + 2);
+
+
+// Tipos de datos en javascript
+// Number => operadores aritméticos
+console.log(100 + 4);
+console.log(100 * 4);
+console.log(100 / 4);
+// modulo => residuo
+console.log(10/3);
+console.log(10%3);
+// string => operador unitario
+console.log(typeof("Hola, mi nombre es Mariana"));
+//typeof()
+console.log(typeof(15));
+//buleans => operadores comparativos
+
+//true(verdadero)
+//false(falso)
+
+console.log(typeof(3<5));
+console.log(10 < 15);
+
+//CONVERSION AUTOMATICA => COERCION DE TIPO
+
+console.log('5'- 1);
+console.log('5'+ 1);
+
+//VARIABLES
+ 
+console.log("Mariana ");
+let age = 18;
+
+let nombre;
+nombre = "Gianmarco";
+console.log(nombre);
+
+nombre = "Cesar";
+console.log(nombre);
+
+nombre = "Wilson";
+console.log(nombre);
+
+const pais = "Peru";
+console.log(pais);
+
+// REGLAS PARA NOMBRAR VARIABLES
+    //1° No debe incluir espacios
+        //let age of friends
+    //2° No pueden ser palabras reservadas
+        //let function = "funcion"
+    //3° No pueden comenzar con números
+        //let 2age = 25
+    //4° No pueden contener signos de puntuacion
+        //let abrir.puerta = "tatata"
+
+//SUGERENCIA
+    //1° Convencion camelCase
+    let firstName = "wilson";
+    console.log(firstName);
+    //2° Convencion snake_case
+    let first_name = "wilson";
+    console.log(first_name);
+    //3° Utiliza nombres en ingles
+    let age5 = 25;
+    //4° utilizar nombres descriptivos
+    let name5 = "Cristopher"
