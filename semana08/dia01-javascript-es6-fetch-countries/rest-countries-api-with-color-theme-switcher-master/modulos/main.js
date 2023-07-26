aab84bf2705f3378cd2d4754bc2ab0390ed75881
Tobias Abregu <a20213174@pucp.edu.pe>
2023-07-26 14:20:29 -0500

    plop

---
+++ semana08/dia01-javascript-es6-fetch-countries/rest-countries-api-with-color-theme-switcher-master/modulos/main.js
@@ -0,0 +1,11 @@
+// import name from "./constants.js"
+// import constants from "./constants.js"
+
+// console.log(name)
+
+// const url = "https://restcountries.com/v3.1/all?fields=name,flags"
+
+// fetch(url)
+//     .then(rpta => rpta)
+//     console.log(rpta)
+console.log("holi");
